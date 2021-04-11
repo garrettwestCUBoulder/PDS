1ed8ab4ff2dfd7c2d99cfd853e6e038accb09798
GarrettwestCUBoulder <garrettwest@me.com>
2021-04-11 11:44:32 -0600

    new port 8080

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.get('/profile', function(req, res){
 
 
 
-var port = process.env.PORT;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
